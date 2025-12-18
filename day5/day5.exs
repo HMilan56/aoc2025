391a40d04c070266e1040933c7d3d8d27bdd243e
HMilan56 <hmilan56@gmail.com>
2025-12-18 22:55:42 +0100

    simplify day5 solution

--- day5/day5.exs
+++ day5/day5.exs
@@ -2,11 +2,17 @@ defmodule Day5 do
   def solve() do
     File.read!("input.txt")
     |> String.split()
-    |> parse_lines()
+    |> Enum.split_while(fn line -> String.contains?(line, "-") end)
+    |> then(fn {ranges, ids} ->
+      {
+        Enum.map(ranges, fn r -> r |> String.split("-") |> Enum.map(&String.to_integer/1) end),
+        Enum.map(ids, &String.to_integer/1)
+      }
+    end)
     |> part2()
   end
 
-  defp part1([ranges, ids]) do
+  defp part1({ranges, ids}) do
     Enum.reduce(ids, 0, fn id, acc ->
       fresh? =
         Enum.any?(ranges, fn [start_id, end_id] ->
@@ -17,39 +23,15 @@ defmodule Day5 do
     end)
   end
 
-  defp part2([ranges, _]) do
-    ranges = Enum.sort_by(ranges, fn [s, _] -> s end)
+  defp part2({ranges, _}) do
+    [first | rest] = Enum.sort(ranges)
 
-    Enum.reduce(tl(ranges), [hd(ranges)], fn [s2, _] = r, acc ->
-      [_, e1] = hd(acc)
-
-      if e1 >= s2 do
-        [merge_ranges(hd(acc), r) | tl(acc)]
-      else
-        [r | acc]
-      end
+    Enum.reduce(rest, [first], fn
+      [s2, e2], [[s1, e1] | acc] when e1 >= s2 -> [[s1, max(e1, e2)] | acc]
+      r, acc -> [r | acc]
     end)
     |> Enum.map(fn [s, e] -> e - s + 1 end)
     |> Enum.sum()
-  end
-
-  defp merge_ranges([s1, e1], [s2, e2]) do
-    if (s1 <= s2 and e1 >= s2) or (s2 <= s1 and e2 >= s1) do
-      [min(s1, s2), max(e1, e2)]
-    end
-  end
-
-  defp parse_lines(lines) do
-    [ranges, ids] = lines |> Enum.chunk_by(&String.contains?(&1, "-"))
-
-    ranges2 =
-      Enum.map(ranges, fn range ->
-        String.split(range, "-") |> Enum.map(&String.to_integer/1)
-      end)
-
-    ids2 = Enum.map(ids, &String.to_integer/1)
-
-    [ranges2, ids2]
   end
 end
 
